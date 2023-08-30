b94e2c6aead3b21ae3c68d3df8d505c253ca8356
Ankit Litouriya <118179332+Ankitlitouriya@users.noreply.github.com>
2023-08-30 06:30:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your code here
+// Game pixel_count is the pixel on the horizontal and vertical axix of the game board (Square)
+const GAME_PIXEL_COUNT = 40;
+const SQUARE_OF_GAME_PIXEL_COUNT= Math.pow(GAME_PIXEL_COUNT,2);
+
+let changeTheDirection = false;
+let totalFoodAte = 0;
+let totalDistanceTravell =0;
+
+const gameContainer = document.getElelmentById("gameContainer")
+
